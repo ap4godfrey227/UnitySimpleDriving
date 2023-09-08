e2926f19b96204dc7d1f845e6ad426292ef61813
ap4godfrey227 <67652648+ap4godfrey227@users.noreply.github.com>
2023-09-07 17:52:12 -0700

    Finished map and started Xp boost system

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -7,12 +8,40 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private TMP_Text highScoreText;
+    [SerializeField] private TMP_Text xpText;
+    [SerializeField] private int maxXpBoost;
+    [SerializeField] private int boostRechargeDuration;
+    
+
+    private int xp;
+
+    private const string XpKey = "XP Boost";
+    private const string XpReadyKey = "XP Boost Ready";
 
     private void Start()
     {
         int highScore = PlayerPrefs.GetInt(ScoreSystem.HighScoreKey, 0);
 
         highScoreText.text = "High Score: " + highScore.ToString();
+
+        xp = PlayerPrefs.GetInt(XpKey, maxXpBoost);
+
+        if(xp == 1)
+        {
+            string xpReadyString = PlayerPrefs.GetString(XpReadyKey, string.Empty);
+
+            if(xpReadyString == string.Empty){ return;}
+
+            DateTime xpReady = DateTime.Parse(xpReadyString);
+
+            if(DateTime.Now > xpReady)
+            {
+                xp = maxXpBoost;
+                PlayerPrefs.SetInt(XpKey, xp);
+            }
+        }
+
+        xpText.text = $"Play/n XP Boost x{xp}";
     }
 
     public void Play()
